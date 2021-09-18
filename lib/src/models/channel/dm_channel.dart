d10b9cf2f4082e7ec4d027502633c746f013e891
Arsenii Liunsha <me@arslee.dev>
2021-09-18 09:07:03 +0900

    Implement DM Channel

---
+++ lib/src/models/channel/dm_channel.dart
@@ -0,0 +1,44 @@
+part of volt;
+
+class DmChannel extends Channel implements MinimalTextChannel {
+  final bool isActive;
+  final Iterable<CacheableUser> recipients;
+  // TODO: replace with cacheable message??
+  final Ulid? lastMessageId;
+
+  DmChannel._new(IVolt client, RawApiMap raw)
+      : isActive = raw['active'] as bool,
+        recipients = [
+          for (final userId in raw['recipients'] as List<String>)
+            CacheableUser._new(client, Ulid(userId))
+        ],
+        lastMessageId = raw['last_message_id'] == null
+            ? null
+            : Ulid(raw['last_message_id'] as String),
+        super._new(client, raw);
+
+  @override
+  Future<Message> fetchMessage(Ulid id) {
+    // TODO: implement fetchMessage
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream fetchMessages(FetchMessagesQueryBuilder query) {
+    // TODO: implement fetchMessages
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream findMessages(SearchMessagesQueryBuilder query) {
+    // TODO: implement findMessages
+    throw UnimplementedError();
+  }
+
+  @override
+  String get mention => '<#${id.toString()}>';
+
+  @override
+  Future<Message> sendMessage(MessageBuilder message) =>
+      client.httpEndpoints.sendMessage(id, message);
+}
--- lib/volt.dart
+++ lib/volt.dart
@@ -50,6 +50,7 @@ part 'src/models/permissions/user_permissions.dart';
 part 'src/models/channel/channel.dart';
 part 'src/models/channel/server_channel.dart';
 part 'src/models/channel/server_text_channel.dart';
+part 'src/models/channel/dm_channel.dart';
 part 'src/models/channel/voice_channel.dart';
 part 'src/models/channel/text_channel.dart';
 part 'src/models/channel/undefined_channel.dart';
