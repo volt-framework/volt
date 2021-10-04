c2d35b5964c241db16921f6d744afc08fa533993
Arsenii Liunsha <me@arslee.dev>
2021-10-04 11:22:49 +0900

    chore(RolePermissions): Make RolePermissions convertable to PermissionsBuilder

--- lib/src/models/role/role_permissions.dart
+++ lib/src/models/role/role_permissions.dart
@@ -1,11 +1,14 @@
 part of volt;
 
 /// Role permissions.
-class RolePermissions {
+class RolePermissions implements Convertable<PermissionsBuilder> {
   final ServerPermissions server;
   final ChannelPermissions channel;
 
   RolePermissions._new(List<dynamic> raw)
       : server = ServerPermissions._new(raw[0] as int),
         channel = ChannelPermissions._new(raw[1] as int);
+
+  @override
+  PermissionsBuilder toBuilder() => PermissionsBuilder.from(this);
 }
