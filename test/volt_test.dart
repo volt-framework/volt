ae8d02aeba3e40a756d1e5d184af78ced17a2ba8
Arseny Liunsha <me@arslee.dev>
2021-10-13 20:36:06 +0900

    feat: Add first unit tests (...of ulids)

--- test/volt_test.dart
+++ test/volt_test.dart
@@ -1,8 +1,22 @@
 import 'package:test/test.dart';
+import 'package:volt/volt.dart';
+
+const ulidAYear = 2020;
+const ulidBYear = 2021;
+
+final ulidA = Ulid.fromTimestamp(DateTime.utc(ulidAYear));
+final ulidB = Ulid.fromTimestamp(DateTime.utc(ulidBYear));
+
+final client = VoltRest('dummy_token');
 
 void main() {
-  group('Test models parsing', () {
-    setUp(() {});
-    test('First Test', () {});
+  group('Test ULIDs', () {
+    test('ULIDs should have correct timestamps', () {
+      expect(ulidA.toTimestamp().year, ulidAYear);
+      expect(ulidB.toTimestamp().year, ulidBYear);
+    });
+    test('ULIDs should be compared correctly', () {
+      expect(ulidA.compareTo(ulidB), -1);
+    });
   });
 }
